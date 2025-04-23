7a60e3f5ecee48c5ed887123f3fa28a0f5c5da07
DanielOliveiray <danieloliveira.programmer@gmail.com>
2025-04-22 23:16:39 -0300

    Pagina ContatoEnviado adicionado

---
+++ src/app/ContatoEnviado/page.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+import Link from 'next/link';
+
+export default function Success() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Mensagem enviada com sucesso!</h1>
+        <p className={styles.text}>Obrigado por entrar em contato com a KSERVICE. Em breve responderemos sua mensagem.</p>
+        <Link href="/" passHref>
+          <button className={styles.button}>Voltar para a página inicial</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
